76901dddff80ca2be61487537b6e1ced0c2b2478
AryanSHAH611 <163873207+AryanSHAH611@users.noreply.github.com>
2025-06-17 01:30:53 +0530

    Add files via upload

---
+++ Basic.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+/*
+    int a = 123;
+
+    cout << a << endl;
+
+    char b ='v';
+
+    cout << b << endl;
+
+    bool b1 = true;
+    cout << b1 << endl;
+
+    float f =1.2;
+    cout << f << endl;
+
+    double d =1.23;
+    cout << d << endl;
+
+    int size = sizeof(a);
+    cout << "Size of a:" << a << endl;
+*/
+/*
+int a = 'a';
+cout << a << endl;
+
+char ch = 98;
+cout << ch << endl;
+
+char ch1 = 12345;
+cout << ch1 << endl;
+
+*/
+/*
+double a = 2.0/5;
+
+cout << a << endl;
+
+cout << 2.0/5 << endl;
+
+*/
+
+int a = 2;
+int b = 3;
+
+bool first = (a==b);
+cout << first << endl;
+
+bool secound = (a<b);
+cout << secound << endl;
+
+bool third = (a>b);
+cout << third << endl;
+
+bool fourth = (a<=b);
+cout << fourth << endl;
+
+bool fivth = (a>=b);
+cout << fivth << endl;
+
+bool sixth = (a!=b);
+cout << sixth << endl;
+ }
