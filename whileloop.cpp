c0449c8affc025d986ecf3fdb40adb6ba9abef7b
AryanSHAH611 <163873207+AryanSHAH611@users.noreply.github.com>
2025-06-29 23:42:46 +0530

    Add files via upload

---
+++ whileloop.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+/*
+{
+    int n ;
+    cin >>n;
+
+    int i = 0;
+
+    while (i<=n)
+    {
+        cout << i <<"";
+        i = i+1;
+    }
+     return 0;
+}
+
+{
+int n;
+cin >> n;
+
+int i = 1;
+int sum = 0;
+
+while (i<=n){
+    sum = sum + i;
+    i = i + 1;
+}
+
+cout<<  "Value of sum is " <<sum<<endl;
+
+}
+*/
+/*
+{
+int n;
+cin >> n;
+
+int i = 2;
+int sum = 0;
+
+while(i<=n)
+{
+sum = sum + i;
+i = i + 1;
+}
+
+cout<< "Value of the th sum is :" <<sum<<endl;
+}
+
+{
+int n ;
+cin >> n;
+
+int i = 2;
+
+while (i<n)
+{
+if(n%i==0){
+    cout<< "Not Prime for " <<i << endl;
+}else{
+    cout <<"Prime for "<<i << endl;
+}
+i = i+1;
+}
+}
+
+*/
+
+/*PATTERN*/
+
