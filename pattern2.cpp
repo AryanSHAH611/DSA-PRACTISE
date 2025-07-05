3d81b650fd765d216e27ce7bfa0dc3ee49a10925
AryanSHAH611 <163873207+AryanSHAH611@users.noreply.github.com>
2025-07-05 23:18:28 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+using namespace std;
+
+int main()
+/* {
+    int n;
+    cin >> n;
+
+    int row = 1;
+    while (row <= n) {
+        int col = 1;
+        while (col <= row) {
+            cout << row;
+            col = col + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    }
+
+    return 0;
+}
+
+{
+    int n;
+    cin>>n;
+
+    int row = 1;
+    while(row<=n){
+        int col = 1;
+        while(col<=row){
+            cout<<row;
+            col = col + 1;
+        }
+        cout<<endl;
+        row = row + 1;
+    }
+}
+
+{
+    int n;
+    cin >> n;
+    int row =1;
+
+    while(row<=n){
+        int col =1;
+        while(col<=row){
+            cout<<"x";
+            col = col + 1;
+        }
+        cout<< endl;
+        row = row +1;
+    }
+}
+
+
+{
+    int n;
+    cin>> n;
+
+    int row =1;
+
+    while(row<=n){
+        int col = 1;
+        while(col<= row){
+            cout<< row ;
+            col = col + 1;
+        }
+        cout<<endl;
+        row = row +1;
+    }
+}
+
+
+{
+    int n;
+    cin>>n;
+    int row = 1;
+    int count =1;
+
+    while(row<= n){
+        int col =1;
+        while(col<=row){
+            cout<<count<<" ";
+            count = count + 1;
+            col = col + 1;
+        }
+        cout<< endl;
+        row = row + 1;
+    }
+}
+
+
+{
+    int n;
+    cin >>n;
+
+    int row = 1;
+    while(row<=n){
+        int col = 1;
+        int count = row;
+        while(col<=row){
+            cout<<count;
+            count = count +1;
+            col = col + 1;
+        }
+        cout<<endl;
+        row = row + 1;
+
+    }
+}
+
+
+{
+    int n;
+    cin >>n;
+    int i = 1;
+    while(i<=n){
+        int j = 1;
+        while(j<=i){
+            cout<<(i-j+1)<<"";
+            j = j+ 1;
+        }
+        cout<<endl;
+        i = i + 1;
+    }
+}
+*/
