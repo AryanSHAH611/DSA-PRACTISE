222296768b10da20035671325cd12ff9c2d3111f
AryanSHAH611 <163873207+AryanSHAH611@users.noreply.github.com>
2025-07-06 00:31:32 +0530

    Add files via upload

---
+++ pattern3.cpp
@@ -0,0 +1,147 @@
+#include<iostream>
+using namespace std;
+
+int main()
+/* {
+    int n;
+    cin >> n;
+
+    int row = 1;
+    while (row <= n) {
+        int col = 1;
+        while (col <= n) {
+            char ch = 'A' + row - 1;
+            cout << ch << " ";
+            col = col + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    }
+
+    return 0;
+}
+
+
+{
+    int n;
+    cin>>n;
+
+    int row = 1;
+    while(row<=n){
+        int col = 1;
+        while(col<= n){
+            char ch = 'A' + col - 1;
+            cout<<ch<<"";
+            col = col + 1;
+        }
+        cout<< endl;
+        row = row + 1;
+    }
+}
+
+
+{
+    int n ;
+    cin >> n;
+    char value = 'A';
+    int row = 1;
+    while(row<=n){
+        int col = 1;
+        while(col<=n){
+            cout<< value;
+            value = value + 1;
+            col = col + 1;
+        }
+        cout<< endl;
+        row = row + 1;
+    }
+}
+
+{
+    int n;
+    cin>> n;
+
+    char value = 'A';
+    int row = 1;
+    while(row<=n){
+        int col = 1;
+        while(col<= n){
+            char value = 'A'+ row + col -2;
+            cout<<value<<" ";
+            col = col+1;
+        }
+        cout<<endl;
+        row = row + 1;
+    }
+}
+
+{
+    int n ;
+    cin>> n;
+    char value = 'A';
+    int row = 1;
+    while (row<=n)
+    {
+        int col = 1;
+        while (col <= row)
+        {
+            char value = 'A' + row -1;
+            cout << value << " ";
+            col = col + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    }
+}
+
+{
+    int n ;
+    cin>> n;
+
+    int row = 1;
+    while(row<=n){
+        int col = 1;
+        while(col<=row){
+            char value = 'A'+ row + col - 2 ;
+            cout<<value;
+            col = col+ 1;
+        }
+        cout<<endl;
+        row = row + 1;
+    }
+}
+
+{
+    int n ;
+    cin >>n;
+    int row = 1;
+    while(row<=n){
+        int col = 1;
+        char start = 'A' + n - row;
+        while(col<=row){
+            cout<<start;
+            start = start + 1;
+            col = col + 1;
+        }
+        cout<<endl;
+        row = row + 1;
+    }
+}
+
+{
+    int n;
+    cin>>n;
+    int row = 1;
+    while(row<=n){
+        int col = 1;
+        char ch = 'A' + row + col - 2;
+        while(col<=n){
+            cout<<ch;
+            ch = ch + 1;
+            col = col + 1;
+        }
+        cout<<endl;
+        row = row +1;
+    }
+}
+*/
