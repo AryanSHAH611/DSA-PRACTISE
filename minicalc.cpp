4abc5b326b99a67eee612da126fc3fd78e8d11f2
AryanSHAH611 <163873207+AryanSHAH611@users.noreply.github.com>
2025-07-25 23:58:37 +0530

    Add files via upload

---
+++ minicalc.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+int main() {
+/*    
+    int a , b;
+    cout << " ENter the vamlue of A: " << endl;
+    cin >> a;
+
+    cout<<"Enter the value of B:" << endl;
+    cin >> b;
+
+    char op;
+    cout << " Enter the operator (+, -, *, /): " << endl;
+    cin >> op;
+
+    switch (op) // Switch statement to handle different operations
+    {
+    case '+': cout << "Result: " << a + b << endl;
+        break;
+    
+    case '-': cout << "Result: " << a- b << endl;
+        break;
+
+    case '*': cout << "Result: " << a * b << endl;
+        break;
+    
+    case '/': cout << "Result: " << a / b << endl;
+        break;
+    
+    default:
+        break;
+    }
+}
+
+int amount  = 1330;
+int note100 = 0;
+int note50 = 0; 
+int note20 = 0;
+int note1 = 0;
+
+switch(01){
+    case 1: 
+note100 = amount /100; // Calculate number of 100s
+amount = amount % 100; // Update amount after extracting 100s
+cout << "100s: " << note100 << endl;
+    case 2:
+
+note50 = amount /50; // Calculate number of 50s
+amount = amount % 50; // Update amount after extracting 50s
+cout << "50s: " << note50 << endl;
+    case 3:
+
+note20 = amount / 20; // Calculate number of 20s
+amount = amount % 20; // Update amount after extracting 20s
+cout << "20s "<< note20 << endl;
+    case 4:
+
+note1 = amount / 1; // Calculate number of 1s
+amount = amount % 1; // Update amount after extracting 1s; 
+cout << "1s: " << note1 << endl;
+break;
+    default:
+        cout << "Invalid case" << endl;
+        break;
+}
+return 0;
+}
+*/
